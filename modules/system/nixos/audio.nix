2fb632e3f4e76e84c0a934603a1f09315c732398
Brett Henderson <BrettsVoid@gmail.com>
2026-07-02 11:45:13 +0100

    nixos: add rtkit for pipewire

--- modules/system/nixos/audio.nix
+++ modules/system/nixos/audio.nix
@@ -1,6 +1,9 @@
 # PipeWire audio stack
 _: {
   flake.modules.nixos.audio = {
+    # rtkit lets PipeWire acquire realtime scheduling priority.
+    security.rtkit.enable = true;
+
     services.pipewire = {
       enable = true;
       alsa.enable = true;
